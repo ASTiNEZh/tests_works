919be3f37bff3f6f84392b0dd349d7cde5e48e82
ASTiNEZh <89325728+ASTiNEZh@users.noreply.github.com>
2021-08-23 05:31:44 +0300

    Update task_1_(connection_to_db).php

--- task_1_(connection_to_db).php
+++ task_1_(connection_to_db).php
@@ -1,8 +1,8 @@
 <?php
 
-$connection = mysqli_connect('127.0.0.1','mysql','','db_for_tnwm');
+$connection = mysqli_connect('127.0.0.1','mysql','','db_for_tnwm'); //подключение к БД ('IP', 'логин', 'пароль', 'имя базы')
 
-if ($connection == false)
+if ($connection == false) //оповещение на случай неудачного подключения
 {
 	echo 'При подключении к базе данных произошла ошибка!<br>';
 	echo mysqli_connect_error();
