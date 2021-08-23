78e716ea5c34ed1b6be77013337564bc8d73faa5
ASTiNEZh <89325728+ASTiNEZh@users.noreply.github.com>
2021-08-23 05:40:11 +0300

    Update task_2_part_1_(feedback).php

--- task_2_part_1_(feedback).php
+++ task_2_part_1_(feedback).php
@@ -7,18 +7,21 @@
 
 	<body>
 
-		<!-- подключение Vue.js -->
+			<!-- подключение Vue.js -->
 		<script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
 
 		<form id="read" method="POST" action="feedback.php">
+				<!-- Поле: ФИО (с проверкой на клиенте через фрейм Vue.js)-->
 			Как Вас зовут?
 			<br>
 			<input v-on:input="read_name ($event.target.value)" type="text" placeholder="ФИО" name="name" required>
 			<br>
+				<!-- Поле: Адрес -->
 			Куда нам слать для Вас валентинки?
 			<br>
 			<input type="text" placeholder="Адрес" name="adres" >
 			<br>
+				<!-- Поле: Контакты (с проверкой на клиенте через фрейм Vue.js)-->
 			Как с Вами можно связатся?
 			<br>
 			<input type="text" placeholder="email" name="email">
@@ -28,6 +31,7 @@
 			<br>
 			Оставьте свой отзыв:
 			<br>
+				<!-- Feedback -->
 			<textarea name="comment" cols="100" rows="10"></textarea>
 			<br>
 			<input type="file" placeholder="Имя файла для отправки" name="file">
@@ -41,7 +45,7 @@
 
 			include('connection_to_db.php'); //подключение к БД
 
-			$name = $_POST['name'];//Извлечение данных из суперглобального массива _POST
+			$name = $_POST['name']; //Извлечение данных из суперглобального массива _POST
 			$adres = $_POST['adres'];
 			$email = $_POST['email'];
 			$dom = $_POST['dom'];
@@ -49,10 +53,11 @@
 			$comment = $_POST['comment'];
 			$file = $_POST['file'];
 
-			if($dom == "@gmail.com"){
+			if($dom == "@gmail.com") //Серверная проверка домена почты
+			{ 
 				exit();
 			}
-			if($name != '' and $phone != "")
+			if($name != '' and $phone != "") //Серверная проверка пустых полей
 			{
 				$count_comment = mysqli_query($connection, "INSERT INTO `feedback` (`comment`, `name`, `adres`, `email`, `phone`, `file`) VALUES ('$comment', '$name', '$adres', '$email', '$phone', '$file')");
 			} else{
